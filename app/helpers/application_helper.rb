fcfa7f66542548bbe713b912a27ce62f4b839e7f
Mark Volosiuk <marchi.martius@gmail.com>
2013-05-29 16:49:43 +0300

    fix some I18n issues. Updated doc-diagrams

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def time_ago_human time
+    t 'time.ago', time: time_ago_in_words(time)
+  end
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -6,7 +6,7 @@ class Client < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true,
                     format: { with: /\A\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
-  validates :phone, format: { with: /\A((8|\+38)-?)?(\(?044\)?)?-?\d{3}-?\d{2}-?\d{2}\z/ }
+  validates :phone, format: { with: /\A((8|\+38)-?)?\s?(\(?0\d{2}\)?)?(-|\s)?\d{3}-?\d{2}-?\d{2}\z/ }
   
   def to_s
     self.name
